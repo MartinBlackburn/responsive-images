c4abb96c346a3bf8c3b5f0590458c8d70450f86c
Martin Blackburn <walking_fishy@hotmail.com>
2013-01-22 17:00:41 +0000

    working, needs more testing

--- js/responsiveImages.js
+++ js/responsiveImages.js
@@ -1,4 +1,38 @@
-ResponsiveImages = function()
-{
-	
+ResponsiveImage = function()
+{   
+    var width = $(document).width();
+    var pattern = new RegExp(/data-image-/);
+            
+    $(".replaceImage").each(function()
+    {
+        var newImageSRC = "";
+        
+        $.each(this.attributes, function(i, attribute)
+        {
+           var name = attribute.name;
+           
+           //test is attribute name starts "data-image-"
+           if(pattern.test(name))
+           {
+               var minShowSize = name.split('-')[2];
+               
+               if(width >= minShowSize)
+               {
+                   newImageSRC = attribute.value;
+               }
+           }
+        });
+        
+        //load new image if needed
+        if(newImageSRC != "")
+        {
+            $(this).replaceWith("<img src='" + newImageSRC + "' />");
+        }
+    });
 };
+
+
+$(function() 
+{
+    new ResponsiveImage();
+});
